31d3ae69b3cab2cdbdf9d7cebdb6c879e2aaa6d5
paceart <pacearthur@gmail.com>
2019-06-25 10:48:29 +0200

    commit

--- Cohesion App/src/containers/DefaultPage/MesProjets/components/Statistics.jsx
+++ Cohesion App/src/containers/DefaultPage/MesProjets/components/Statistics.jsx
@@ -4,22 +4,12 @@ import {
 } from 'recharts';
 import LoadingIcon from 'mdi-react/LoadingIcon';
 
-const data01 = [{ name: 'React/Redux', value: 40, fill: '#4ce1b6' },
-  { name: 'AWS', value: 175, fill: '#70bbfd' },
-  { name: 'Architecture', value: 70, fill: '#ff4861' },
-  { name: 'Machine Learning', value: 70, fill: '#7e6bc9' }];
+const data01 = [{ name: 'Javascript', value: 54, fill: '#70bbfd' },
+  { name: 'Communication', value: 23, fill: '#ff4861' },
+  { name: 'Machine Learning', value: 23, fill: '#7e6bc9' }];
 
 const data012 = [{ name: 'React/Redux', value: 40, fill: '#4ce1b6' },
-  { name: 'AWS', value: 175, fill: '#70bbfd' },
-  { name: 'Machine Learning', value: 70, fill: '#7e6bc9' }];
-
-const data02 = [{ name: 'React/Redux', value: 40, fill: '#4ce1b6' },
-  { name: 'AWS', value: 175, fill: '#70bbfd' },
-  { name: 'Architecture', value: 70, fill: '#ff4861' },
-  { name: 'Machine Learning', value: 70, fill: '#7e6bc9' }];
-
-const data022 = [{ name: 'React/Redux', value: 40, fill: '#4ce1b6' },
-  { name: 'AWS', value: 175, fill: '#70bbfd' },
+  { name: 'Javascript', value: 175, fill: '#70bbfd' },
   { name: 'Machine Learning', value: 70, fill: '#7e6bc9' }];
 
 const style = {
@@ -36,24 +26,22 @@ export default class Statistics extends PureComponent {
     this.state = {
       update: false,
       data1: data01,
-      data2: data02,
     };
 
     this.onUpdate = this.onUpdate.bind(this);
   }
 
   onUpdate() {
-    const { data1: stateData1, data2: stateData2 } = this.state;
+    const { data1: stateData1 } = this.state;
 
     this.setState({ update: true });
     // your async logic here
     const data1 = stateData1 === data01 ? data012 : data01;
-    const data2 = stateData2 === data02 ? data022 : data02;
-    setTimeout(() => this.setState({ update: false, data1, data2 }), 2000);
+    setTimeout(() => this.setState({ update: false, data1 }), 2000);
   }
 
   render() {
-    const { update, data1, data2 } = this.state;
+    const { update, data1 } = this.state;
 
     return (
       <div className="project-summary__statistics">
@@ -63,14 +51,6 @@ export default class Statistics extends PureComponent {
           <PieChart width={240} height={220}>
             <Tooltip />
             <Pie data={data1} dataKey="value" cx={110} cy={110} innerRadius={60} outerRadius={80} label />
-            <Legend layout="vertical" verticalAlign="middle" wrapperStyle={style} />
-          </PieChart>
-        </div>
-        <div className="project-summary__statistic">
-          <p className="project-summary__statistic-title">Votre Apport</p>
-          <PieChart width={240} height={250}>
-            <Tooltip />
-            <Pie data={data2} dataKey="value" cx={110} cy={110} innerRadius={60} outerRadius={80} label />
             <Legend layout="vertical" verticalAlign="middle" wrapperStyle={style} />
           </PieChart>
         </div>
